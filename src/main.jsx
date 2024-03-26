f5d2c7c9a30572da139a10952619abe72ffdde80
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-26 12:11:55 +0400

    h1-in icerisinde

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<div>salam react qardas</div>)
+root.render(<div>
+<h1>salam react qardas</h1>
+</div>)
